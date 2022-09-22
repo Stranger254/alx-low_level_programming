05761e8a610b8a183c5bf13dad88e8c9a2f17f8b
Stranger254 <muhiatomkys@gmail.com>
2022-09-22 08:34:28 +0000

    Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,15 @@
  * _strcmp -> for comparing purpose
  * @s1: param1
  * @s2: param2
- * Return: integer
+ * Return: Always 0 (Success)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
-
-	result = strcmp(s1, s2);
-	return (result);
+	int i;
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
