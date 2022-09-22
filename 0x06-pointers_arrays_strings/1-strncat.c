1d123213835b3f19b76e6f033db143e3607349b1
Stranger254 <muhiatomkys@gmail.com>
2022-09-22 08:06:35 +0000

    Strngcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat -> function to append some charx
  * @dest: first param
  * @src: second param
- * @n:3rd param
+ * @n: i3rd param
  * Return: string
  */
 char *_strncat(char *dest, char *src, int n)
