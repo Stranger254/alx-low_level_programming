f79a6631424326811ca40796dbcb755a64b5bca8
Stranger254 <muhiatomkys@gmail.com>
2022-10-04 08:13:06 +0000

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -> function to get length of a string
+ * @s: string pointer to be passed to this function
+ * Return: returns length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
