c20db79e8d8eba79a5e7524e4169964e96b57c14
Stranger254 <muhiatomkys@gmail.com>
2022-10-04 10:10:18 +0000

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,7 +12,7 @@ int main(int argc, __attribute__((unused)) char *argv[])
 	int i;
 	for (i = 0; i < argc; i++)
 	{
+		printf("%d\n", i - 1);
+		return (0);
 	}
-	printf("%d\n", i - 1);
-	return (0);
 }
