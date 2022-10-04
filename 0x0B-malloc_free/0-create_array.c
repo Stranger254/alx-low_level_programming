6f3f122d31d5fe418d288d94d39021587021b75b
Stranger254 <muhiatomkys@gmail.com>
2022-10-04 08:48:44 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int index;
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) *size);
+	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 	for (index = 0; index < size; index++)
