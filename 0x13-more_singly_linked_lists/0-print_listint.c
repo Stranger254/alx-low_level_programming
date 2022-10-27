b50e4030cab19c12c4edf5e310f03090f89be93e
Stranger254 <muhiatomkys@gmail.com>
2022-10-27 00:31:16 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
-	size_t count = 0;
+	size_t nnodes = 0;
 
-	while (cursor != NULL)
+	while (h != NULL)
 	{
-		pritnf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		pritnf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
 	}
-	return (count);
+	return (nnodes);
 }
